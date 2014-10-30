7a79028326a7a6000a93c14060dcb78fe83cba07
Joe Burgess <joe@flatironschool.com>
2014-10-30 12:35:10 -0400

    Initial Commit

---
+++ zenGarden/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  zenGarden
+//
+//  Created by iOS Staff on 10/30/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
